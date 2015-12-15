7619801592b2263bb20ac974e81b0012670ef755
maozillah <kayexmao@gmail.com>
2015-12-15 16:18:32 -0500

    working filters

--- scripts/show_class.inc.php
+++ scripts/show_class.inc.php
@@ -10,24 +10,24 @@ if (isset($_GET['q'])) {
 
 echo $q;
 
-// $directory = 'uploads/';
-// $db = ConnectToDB();
+$directory = 'uploads/';
+$db = ConnectToDB();
 
-// $sql = "SELECT project.img_url, project.project_name, project.project_description
-//     FROM project
-//         JOIN class
-//             ON project.class_id = class.class_id
-// WHERE class_year = '$q'";
-// $result = $db->query($sql);
+$sql = "SELECT project.img_url, project.project_name, project.project_description
+    FROM project
+        JOIN class
+            ON project.class_id = class.class_id
+		WHERE class_name = '$q'";
+$result = $db->query($sql);
 
-// if ($result->num_rows > 0) {
-//     while ($row = $result->fetch_assoc()) {
-//         echo $row['project_name'];
-//         echo $row['project_description'];
-//         echo '<img src="' . $directory . $row['img_url'] . '" alt="">';
-//     }
-// }
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo $row['project_name'];
+        echo $row['project_description'];
+        echo '<img src="' . $directory . $row['img_url'] . '" alt="">';
+    }
+}
 
-// $result->close();
-// mysqli_close($db);
+$result->close();
+mysqli_close($db);
 ?>
