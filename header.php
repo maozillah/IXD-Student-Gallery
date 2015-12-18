58d49900d9b087fd179538863c865795e8d186a5
maozillah <kayexmao@gmail.com>
2015-12-17 23:29:48 -0500

    link to show gallery

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">IxD Gallery</a>
+            <a class="navbar-brand" href="show_gallery.php">IxD Gallery</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <div class="navbar-form navbar-right">
